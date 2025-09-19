3c727c15db836f60a785661c02ce71aea89af635
Sumedh Badnore <sumedhbadnore@Sumedhs-MacBook-Air.local>
2025-09-19 17:11:58 -0400

    Disabled ESLint for build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+// next.config.js
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: { ignoreDuringBuilds: true },
+
 };
 
 export default nextConfig;
